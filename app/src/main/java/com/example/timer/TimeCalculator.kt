dac73d879f2ceeff9785c6a1d65f044865193682
utkarsh-pwlive <utkarsh.saxena@pw.live>
2026-02-26 17:02:02 +0530

    ui fix

--- app/src/main/java/com/example/timer/TimeCalculator.kt
+++ app/src/main/java/com/example/timer/TimeCalculator.kt
@@ -9,8 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -57,7 +57,7 @@ fun TimeCalculator(vm: ShiftViewModel = viewModel()) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .navigationBarsPadding()
+            .safeDrawingPadding()
     ) {
 
         Column(
